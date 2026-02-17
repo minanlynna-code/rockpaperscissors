8bc37a4a0b320e1f041a017478f7422117c225a9
minanlynna-code <minanlynna@gmail.com>
2026-02-17 16:28:06 +0700

    Score, message for choice, and final result.

--- rockpaperscissors1.py
+++ rockpaperscissors1.py
@@ -1,33 +1,48 @@
 import random
+
 print("Welcome to The Game...")
-choice = ["Rock", "Scissors","Paper"]
-computer_choice = random.choice(choice)
-round = 1
-while round <= 3:
-    print("rock scissors paper")
-    user_choice = input("Your Choice is: ")
-    if(computer_choice == user_choice):
-        print("Tie")  
+
+choices = ["rock", "scissors","paper"]
+
+round_num = 1
+user_score = 0
+computer_score = 0
+
+while round_num <= 3:
+    print(f"\n--- Round {round_num} ---")
+    computer_choice = random.choice(choices)
+
+    user_choice = input("Choose (rock/scissors/paper): ").lower()
+
+    if user_choice not in choices:
+        print("Invalid choice! Try again.")
+        continue
+
+    print("Computer chose:", computer_choice)
+
+    if computer_choice == user_choice:
+        print("It's a Tie!")
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "scissors" and computer_choice == "paper") or
+        (user_choice == "paper" and computer_choice == "rock")
+    ):
+        print("You Win this round!")
+        user_score += 1
     else:
-        if(computer_choice == choice[0]):
-            if(user_choice == choice[1]):
-                print("You Lost!")
-            elif(user_choice == choice[2]):
-                print("You win!") 
-        elif(computer_choice == choice[1]):
-            if(user_choice == choice[0]):
-                print("You win!")
-            elif(user_choice == choice[2]):
-                print("You Lost!")
-        elif(computer_choice == choice[2]):
-            if(user_choice == choice[0]):
-                print("You Lost!")
-            elif(user_choice == choice[1] ):
-                print("You win!")
-    print("The computer choice is " + computer_choice)
-    round += 1
-print ("Thank for your play! Try next time!")
+        print("You Lose this round!")
+        computer_score += 1
 
+    round_num += 1
 
+# Final Result
+print("\n===== Final Result =====")
+print("Your Score:", user_score)
+print("Computer Score:", computer_score)
 
-
+if user_score > computer_score:
+    print("You Won The Game!")
+elif computer_score > user_score:
+    print("Computer Won The Game!")
+else:
+    print("The Game is a Tie!")
